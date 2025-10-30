3966b578e982348c0fe28261b22eaf1d414bb489
alielbekov <eldorbek2001@arizona.edu>
2025-10-29 23:21:43 -0400

    add domains to the vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	  server: {
+    host: true,
+    allowedHosts: ['https://alielbekov.dev', 'https://www.alielbekov.dev', 'http://localhost:3000']
+  },
+  preview: {
+    allowedHosts: ['https://alielbekov.dev', 'https://www.alielbekov.dev', 'http://localhost:3000']
+  },
 	plugins: [tailwindcss(), sveltekit()]
 });
