e786a3fba18f530684f45b24cae92458f01119a2
Ali Elbekov <eldorbekclash@gmail.com>
2026-04-14 10:08:39 +0000

    Fix Universal Principles of UX Amazon link

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -34,7 +34,7 @@
         <h2 class="text-xl md:text-2xl font-semibold mb-2">Currently</h2>
         <ul class="list-disc pl-6 space-y-1">
             <li class="break-words">Reading <a class="text-blue-500 underline hover:text-green-600" href="https://www.amazon.com/Scalability-Startup-Engineers-Artur-Ejsmont/dp/0071843655" target="_blank">Scalability Rules by Artur Ejsmont</a></li>
-            <li class="break-words">Reading <a class="text-blue-500 underline hover:text-green-600" href="https://www.amazon.com/Universal-Principles-UX-Strategies-Interactions/dp/0760399661" target="_blank">Universal Principles of UX: 100 Timeless Strategies to Create Positive Interactions Between People and Technology</a></li>
+            <li class="break-words">Reading <a class="text-blue-500 underline hover:text-green-600" href="https://www.amazon.com/Universal-Principles-Strategies-Interactions-Technology/dp/0760378045" target="_blank">Universal Principles of UX: 100 Timeless Strategies to Create Positive Interactions Between People and Technology</a></li>
             <li class="break-words"><s>Understanding Cisco Networking Technologies (Completed!)</s></li>
             <li class="break-words">Tinkering with <a class="text-blue-500 underline hover:text-green-600" href="https://n8n.io" target="_blank">n8n</a></li>
             <li class="break-words">Looking into <a class="text-blue-500 underline hover:text-green-600" href="https://platform.openai.com/docs/guides/realtime" target="_blank">Realtime Audio API by ChatGPT</a></li>
